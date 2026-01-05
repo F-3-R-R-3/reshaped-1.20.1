acc8cb5bdc6fafd6f38cd1bcbe02df5432df9f8f
F-3-R-R-3 <ferrerobeyns@icloud.com>
2026-01-05 18:45:55 +0100

    Initial commit

---
+++ src/main/java/net/f3rr3/reshaped/ReshapedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.f3rr3.reshaped;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ReshapedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
