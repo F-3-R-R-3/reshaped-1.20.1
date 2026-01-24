9421e5948719705e7e27e3fd2c936f47e5d7875c
F-3-R-R-3 <ferrerobeyns@icloud.com>
2026-01-24 15:39:51 +0100

    removed unused imports

--- src/main/java/net/f3rr3/reshaped/client/gui/ConfigScreen/ModMenuIntegration.java
+++ src/main/java/net/f3rr3/reshaped/client/gui/ConfigScreen/ModMenuIntegration.java
@@ -1,8 +1,6 @@
 package net.f3rr3.reshaped.client.gui.ConfigScreen;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import net.minecraft.client.gui.screen.Screen;
-import net.f3rr3.reshaped.client.gui.RadialMenuScreen;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
