10719ee5dc14aafbc2c3bb1d60341acd4fb95b36
F-3-R-R-3 <ferrerobeyns@icloud.com>
2026-02-09 13:49:00 +0100

    chadded a config using cloth config API

---
+++ src/main/java/net/f3rr3/reshaped/client/gui/ConfigScreen/ModConfig.java
@@ -0,0 +1,12 @@
+package net.f3rr3.reshaped.client.gui.ConfigScreen;
+
+public class ModConfig {
+    public static boolean enableDevMode = false;
+    public static boolean enableRadialItemBadges = true;
+    public static boolean enableWaxedItemBadges = true;
+    public static int RadialResolution = 5;
+
+    public static void save() {
+    }
+}
+
--- src/main/java/net/f3rr3/reshaped/client/gui/ConfigScreen/ModMenuIntegration.java
+++ src/main/java/net/f3rr3/reshaped/client/gui/ConfigScreen/ModMenuIntegration.java
@@ -5,6 +5,6 @@ import com.terraformersmc.modmenu.api.ModMenuApi;
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ConfigScreen::new;
+        return ConfigScreen::create;
     }
 }
